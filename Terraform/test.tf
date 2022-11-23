6c20b58ec0a0c07008ec79811fbd0c875a1f0abc
sanghwan park <psh000987@naver.com>
2022-11-23 11:44:19 +0900

    azure - tf - vnet OK

--- Terraform/test.tf
+++ Terraform/test.tf
@@ -1,3 +1,4 @@
+# 테라폼 처음 시작시 공급자 설정 & 테라폼 버전 설정
 terraform {
   required_providers {
     azurerm = {
@@ -14,8 +15,34 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   location = "Korea Central"
-  name     = "Hamster"
+  name     = "RG-sang"
 }
 
+resource "azurerm_network_security_group" "sg" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-dddd
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-sang"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.sg.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
